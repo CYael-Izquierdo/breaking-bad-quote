7f152ea596571434ccf590a39d6215cb60c5196b
Yael Izquierdo <welowid@gmail.com>
2020-06-23 20:50:15 -0300

    Breaking Bad Quotes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import styled from "@emotion/styled";
+import Quote from "./components/Quote";
+
+const apiUrl = "http://breaking-bad-quotes.herokuapp.com/v1/quotes";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007D35 0%, #007D35 40%, #0f574E 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #FFF;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+  
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [quote, setQuote] = useState({});
+    const [loading, setLoading] = useState(false);
+
+    const getQuote = async () => {
+        setLoading(true);
+        const request = await fetch(apiUrl);
+        const [quote] = await request.json();
+        setQuote(quote);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getQuote();
+    }, []);
+
+    return (
+        <Container>
+            <Quote
+                loading={loading}
+                quoteObject={quote}
+            />
+            <Button
+                onClick={getQuote}
+            >
+                Get Quote
+            </Button>
+        </Container>
+    );
 }
 
 export default App;
